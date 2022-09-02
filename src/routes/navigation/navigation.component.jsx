5ad6b235ac080f17a0a2fdec23fa486e9b6bf270
BitFlippa27 <32537389+BitFlippa27@users.noreply.github.com>
2022-09-02 16:16:12 -0400

    changed to redux and added user.reducer

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,19 @@
 import { Outlet, Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 import { Fragment, useContext } from "react";
 import { ReactComponent as Logo } from "../../assets/logo.svg"
 import { logOutUser } from "../../utils/firebase/firebase.utils.js";
-import { UserContext } from "../../contexts/user.context";
 import { CartContext } from "../../contexts/cart.context";
+import { selectCurrentUser } from "../../store/user/user.selector";
 import CartIcon from "../../components/cart-icon/cart-icon.components";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { NavigationContainer, LogoContainer, NavLinks, NavLink } from "./navigation.styles";
 
 const Navigation = () => {
-  const { currentUser } = useContext(UserContext);
+  const currentUser = useSelector(selectCurrentUser);
   const { isCartOpen } = useContext(CartContext);
 
-  
   return (
     <Fragment>
       <NavigationContainer>
