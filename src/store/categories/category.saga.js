012510a40dde138786f6684b84198bce84caabac
BitFlippa27 <32537389+BitFlippa27@users.noreply.github.com>
2023-08-29 13:15:02 +0200

    sagas deep dive

--- src/store/categories/category.saga.js
+++ src/store/categories/category.saga.js
@@ -1,23 +1,21 @@
-import { takeLatest, all, call, put, take } from "redux-saga/effects";
+import { takeLatest, all, call, put } from "redux-saga/effects"; 
 import { getCategoriesAndDocs } from "../../utils/firebase/firebase.utils";
-
 import { fetchCategoriesSuccess, fetchCategoriesFailed } from "./category.action";
-
 import { CATEGORIES_ACTION_TYPES } from "./category.types"
 
 
 function* fetchCategoriesAsync() {
+  //as async await
   try {
-    //as async await
-    const categoriesArray = yield call(getCategoriesAndDocs, "categories");
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield call(getCategoriesAndDocs, "categories"); //call -> turn function into effect
+    yield put(fetchCategoriesSuccess(categoriesArray)); //dispatch
   } 
   catch (error) {
-    //as dispatch
     yield put((fetchCategoriesFailed(error)));
   }
 }
 
+//LISTENER: the moment you hear FETCH_CATEGORIES_START run async code
 export function* onFetchCategories() {
   //takes just the latest action if multiple same actions happened before and cancels previous ones
   yield takeLatest(CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START, fetchCategoriesAsync); 
@@ -25,5 +23,7 @@ export function* onFetchCategories() {
 
 export function* categoriesSaga() {
   //Saga aggregator listens to sagas
+  //pauses at yield
+  //completes when everything is done like Promise.all
   yield all([call(onFetchCategories)]);
 }
